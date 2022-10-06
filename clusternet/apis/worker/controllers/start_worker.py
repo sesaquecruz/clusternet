fbb08895c4ae1136fc9e19919797de594ccdb41c
EsauM10 <esau2096@gmail.com>
2022-10-06 13:48:51 -0300

    refactor: move container creation to ContainernerWorker

--- clusternet/apis/worker/controllers/start_worker.py
+++ clusternet/apis/worker/controllers/start_worker.py
@@ -1,9 +1,7 @@
 import socket
 from typing import Any, Dict, List
 
-from mininet.node import RemoteController
-
-from clusternet.apis.models.container import ContainerModel
+from clusternet.apis.models import ContainerModel
 from clusternet.apis.presentation.exceptions import BadRequest, NotFound
 from clusternet.apis.presentation.helpers import bad_request, created, error, internal_server_error, not_found, validate_required_params
 from clusternet.apis.presentation.protocols import Controller, HttpRequest, HttpResponse
@@ -17,33 +15,12 @@ class StartWorkerController(Controller):
     
 
     def validate_containers(self, data: Dict[str, Any]) -> List[ContainerModel]:        
-        containers = []
-        
-        for item in data['containers']:
-            containers.append(ContainerModel.from_dict(item))
+        containers = [ContainerModel.from_dict(item) for item in data['containers']]
 
         if(not containers):
             raise Exception(f'Expecting at least one container')
         return containers
     
-
-    def start_containernet(self, request: HttpRequest, containers: List[ContainerModel]):
-        switch_name     = request.get('switch')
-        controller_ip   = request.get('controller_ip')
-        controller_port = request.get('controller_port')
-
-        self.net.addController(name='c0', controller=RemoteController, ip=controller_ip, port=controller_port)
-        self.net.addSwitch(name=switch_name)
-
-        for container in containers:
-            if(container.name in self.net): continue
-
-            container_params = container.to_dict()
-            container_params.pop('name')
-            self.net.addDocker(container.name, **container_params)
-            self.net.addLink(container.name, switch_name)
-        self.net.start()
-
 
     def handle(self, request: HttpRequest) -> HttpResponse:   
         required_params = ['switch', 'controller_ip', 'controller_port', 'containers']
@@ -55,7 +32,7 @@ class StartWorkerController(Controller):
             validate_required_params(request, required_params)
             
             containers = self.validate_containers(request.body)
-            self.start_containernet(request, containers)
+            self.net.start(request, containers)
 
         except BadRequest as ex:
             return bad_request(error(f'{ex}'))
--- clusternet/apis/worker/services.py
+++ clusternet/apis/worker/services.py
@@ -1,5 +1,10 @@
+from typing import List
+
 from mininet.net import Containernet
-from mininet.node import Host
+from mininet.node import Host, RemoteController
+
+from clusternet.apis.models.container import ContainerModel
+from clusternet.apis.presentation.protocols import HttpRequest
 
 
 class ContainernetWorker(Containernet):
@@ -10,10 +15,28 @@ class ContainernetWorker(Containernet):
     def getHost(self, name: str) -> Host:
         return self[name]
 
-    def start(self):
+    def create_containers(self, switch: str, containers: List[ContainerModel]):
+        for container in containers:
+            if(container.name in self): continue
+
+            container_params = container.to_dict()
+            container_params.pop('name')
+            self.addDocker(container.name, **container_params)
+            self.addLink(container.name, switch)
+
+
+    def start(self, request: HttpRequest, containers: List[ContainerModel]):
+        switch_name     = request.body['switch']
+        controller_ip   = request.body['controller_ip']
+        controller_port = request.body['controller_port']
+
+        self.addController(name='c0', controller=RemoteController, ip=controller_ip, port=controller_port)
+        self.addSwitch(name=switch_name)
+        self.create_containers(switch_name, containers)
+        super().start()
         self.is_running = True
-        return super().start()
     
+
     def stop(self):
         self.is_running = True
         return super().stop()
