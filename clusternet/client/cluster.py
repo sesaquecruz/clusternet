ae4dc09f3e658650c60a8560f97ee949d7b34963
EsauM10 <esau2096@gmail.com>
2022-10-07 17:05:50 -0300

    refactor: rename cluster url param

--- clusternet/client/cluster.py
+++ clusternet/client/cluster.py
@@ -6,27 +6,27 @@ from clusternet.client.container import RemoteContainer
 from clusternet.client.worker import RemoteWorker
 
 class Cluster:
-    def __init__(self, cluster_url: str) -> None:
-        self.cluster_url = cluster_url
+    def __init__(self, url: str) -> None:
+        self.url = url
         self.workers: List[RemoteWorker] = []
         self.client = httpx.Client()
 
     def create_worker(self, name: str, ip: str, controller: str, port: int) -> RemoteWorker:
         data = {'name': name, 'ip': ip, 'controller_ip': controller, 'controller_port': port}
-        response = self.client.post(url=f'{self.cluster_url}/workers', json=data)
+        response = self.client.post(url=f'{self.url}/workers', json=data)
         
         if(response.is_error):
             raise Exception(response.json()['error'])
         
         print(f'** {response.json()["content"]}')
-        worker = RemoteWorker(cluster=self.cluster_url, **data)
+        worker = RemoteWorker(cluster=self.url, **data)
         self.workers.append(worker)
         return worker
 
 
     def create_tunnel(self, worker1: RemoteWorker, worker2: RemoteWorker):
         data = {'remote_worker': worker2.name}
-        response = self.client.post(url=f'{self.cluster_url}/workers/{worker1.name}/tunnel', json=data)
+        response = self.client.post(url=f'{self.url}/workers/{worker1.name}/tunnel', json=data)
 
         if(response.is_error):
             raise Exception(response.json()['error'])
@@ -34,28 +34,28 @@ class Cluster:
 
 
     def get_container(self, name: str) -> RemoteContainer:
-        response = self.client.get(url=f'{self.cluster_url}/containers/{name}')
+        response = self.client.get(url=f'{self.url}/containers/{name}')
         if(response.is_error):
             raise Exception(response.json()['error'])
 
         data = response.json()['content']
-        return RemoteContainer(self.cluster_url, ContainerModel.from_dict(data))
+        return RemoteContainer(self.url, ContainerModel.from_dict(data))
 
 
     def get_containers(self) -> List[RemoteContainer]:
-        response = self.client.get(url=f'{self.cluster_url}/containers')
+        response = self.client.get(url=f'{self.url}/containers')
         if(response.is_error):
             raise Exception(response.json()['error'])
         
         containers = response.json()['content']
         return [
-            RemoteContainer(self.cluster_url, ContainerModel.from_dict(data))
+            RemoteContainer(self.url, ContainerModel.from_dict(data))
             for data in containers
         ]
     
 
     def remove(self, worker: RemoteWorker):
-        response = self.client.delete(url=f'{self.cluster_url}/workers/{worker.name}')
+        response = self.client.delete(url=f'{self.url}/workers/{worker.name}')
         
         if(response.is_error):
             raise Exception(response.json()['error'])
