30ec72d90fa9fade215ef8e9c039d388669612f0
EsauM10 <esau2096@gmail.com>
2022-10-22 13:37:16 -0300

    refactor: remove methods startContainers and startTunnels

--- clusternet/apis/worker/services.py
+++ clusternet/apis/worker/services.py
@@ -1,11 +1,5 @@
-from typing import List
-
 from mininet.net import Containernet
-from mininet.node import Host, RemoteController
-
-from clusternet.apis.models import ContainerModel, TunnelModel
-from clusternet.apis.presentation.protocols import HttpRequest
-
+from mininet.node import Host
 
 class ContainernetWorker(Containernet):
     def __init__(self, **params):
@@ -15,35 +9,13 @@ class ContainernetWorker(Containernet):
     def getHost(self, name: str) -> Host:
         return self[name]
 
-    def startContainers(self, switch: str, containers: List[ContainerModel]):
-        for container in containers:
-            if(container.name in self): continue
-
-            container_params = container.to_dict()
-            container_params.pop('name')
-            self.addDocker(container.name, **container_params)
-            self.addLink(container.name, switch)
-
-    def startTunnels(self, switch: str, tunnels: List[TunnelModel]):
-        for tunnel in tunnels:
-            self.getHost(switch).cmd(tunnel.command)
-
-    def start(self, request: HttpRequest, containers: List[ContainerModel], tunnels: List[TunnelModel]):
-        switch_name     = request.body['switch']
-        controller_ip   = request.body['controller_ip']
-        controller_port = request.body['controller_port']
-
-        self.addController(name='c0', controller=RemoteController, ip=controller_ip, port=controller_port)
-        self.addSwitch(name=switch_name)
-        self.startContainers(switch_name, containers)
+    def start(self):
         super().start()
-        self.startTunnels(switch_name, tunnels)
         self.is_running = True
-    
 
     def stop(self):
-        self.is_running = True
-        return super().stop()
+        super().stop()
+        self.is_running = False
 
 
 worker = ContainernetWorker(topo=None, build=False)
