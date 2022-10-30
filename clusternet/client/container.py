abd468895240a7d0fa07897286ee7d4e10e0fcc9
EsauM10 <esau2096@gmail.com>
2022-10-29 21:22:09 -0300

    feat: add method update_cpu to client

--- clusternet/client/container.py
+++ clusternet/client/container.py
@@ -11,9 +11,9 @@ class RemoteContainer:
         response = httpx.post(url=f'{self.url}/hosts/{self.name}/cmd', json=data, timeout=None)
         
         if(response.is_error):
-            raise Exception(response.json()['error'])
-        
+            raise Exception(response.json()['error']) 
         return response.json()['content']
+    
     
     def config_default(self):
         response = httpx.get(url=f'{self.url}/hosts/{self.name}/config', timeout=None)
@@ -21,4 +21,13 @@ class RemoteContainer:
         if(response.is_error):
             raise Exception(response.json()['error'])
         print(f'{response.json()["content"]}')
+    
 
+    def update_cpu(self, cpu_quota: int, cpu_period: int):
+        data = {'cpu_quota': cpu_quota, 'cpu_period': cpu_period}
+        response = httpx.put(url=f'{self.url}/containers/{self.name}/cpu', json=data, timeout=None)
+
+        if(response.is_error):
+            raise Exception(response.json()['error'])
+        print(f'{response.json()["content"]}')
+    
