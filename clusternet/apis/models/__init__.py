8569395c5cd1f01e763e03f1c5c3a5c3750dd717
EsauM10 <esau2096@gmail.com>
2023-04-14 15:05:25 -0300

    refactor: remove unused cluster files

--- clusternet/apis/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from clusternet.apis.models.container import AddContainerModel, ContainerModel
-from clusternet.apis.models.tunnel import TunnelModel, AddTunnelModel
-from clusternet.apis.models.worker import AddWorkerModel, WorkerModel
--- clusternet/apis/models/helpers.py
+++
@@ -1,8 +0,0 @@
-import re
-
-IPV4_ADDRESS = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-
-def get_ipv4_address(ip: str) -> str:
-    if(re.match(IPV4_ADDRESS, ip) is None):
-        raise Exception('IP does not match a IPV4 address')
-    return ip
